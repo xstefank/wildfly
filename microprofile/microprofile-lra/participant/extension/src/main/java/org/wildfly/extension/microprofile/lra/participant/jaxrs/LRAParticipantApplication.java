01d37a1bdd0fc721e38ff7ca3121f19ef16eafa7
xstefank <xstefank122@gmail.com>
2023-01-11 13:15:10 +0100

    LRA missing participant and context tests

---
+++ microprofile/microprofile-lra/participant/extension/src/main/java/org/wildfly/extension/microprofile/lra/participant/jaxrs/LRAParticipantApplication.java
@@ -0,0 +1,38 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2023, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.wildfly.extension.microprofile.lra.participant.jaxrs;
+
+import io.narayana.lra.client.internal.proxy.nonjaxrs.LRAParticipantResource;
+import jakarta.ws.rs.core.Application;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class LRAParticipantApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return new HashSet<>(Arrays.asList(LRAParticipantResource.class));
+    }
+
+}
